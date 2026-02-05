6f850d6656c3a7d9aa0f61c22f568c2b08a3f36a
Ronit Verma <89890505+ronver1@users.noreply.github.com>
2026-02-04 20:04:38 -0600

    Add files via upload

---
+++ ECE319K_Lab1.s
@@ -0,0 +1,123 @@
+// ****************** ECE319K_Lab1.s ***************
+// Your solution to Lab 1 in assembly code
+// Author: Ronit Verma
+// Last Modified: 1/23/26
+// Spring 2026
+        .data
+        .align 2
+// Declare global variables here if needed
+// with the .space assembly directive
+
+        .text
+        .thumb
+        .align 2
+        .global EID
+EID:    .string "RV24923" // Set my EID to the target EID
+
+        .global Phase
+        .align 2
+Phase:  .long 10 
+// Phase= 0 will display your objective and some of the test cases, 
+// Phase= 1 to 5 will run one test case (the ones you have been given)
+// Phase= 6 to 7 will run one test case (the inputs you have not been given)
+// Phase=10 will run the grader (all cases 1 to 7)
+        .global Lab1
+// Input: R0 points to the list
+// Return: R0 as specified in Lab 1 assignment and terminal window
+// According to AAPCS, you must save/restore R4-R7
+// If your function calls another function, you must save/restore LR
+
+Lab1: 
+        PUSH {R1-R7,LR}
+                                // R0 is the pointer to the start of the array
+        LDR R1, [R0]            // R1 now holds the EID in the array
+        MOVS R5, #0
+
+EID_notfound:
+        CMP R1, #0              // Check if struct is terminated
+        BEQ No_EID
+        B EID_one
+
+EID_one:
+        LDRB R2, [R1, R5]       // First letter in R2 and R5 has the offset
+
+Check_End:       
+        CMP R2, #0              // Check is string is terminated
+        BEQ Next_EID
+        B Try_R
+        
+Try_R:
+        CMP R2, #82             // Compares to "R"
+        BEQ Try_V
+        B Next_EID
+Try_V: 
+        ADDS R5, #1             // Next characterr
+        LDRB R2, [R1, R5]
+        CMP R2, #86             // Compares to "V"
+        BEQ Try_two_one
+        B Next_EID
+Try_two_one:
+        ADDS R5, #1             // Next character
+        LDRB R2, [R1, R5]
+        CMP R2, #50             // Compares to "2"
+        BEQ Try_four
+        B Next_EID
+Try_four:
+        ADDS R5, #1             // Next character
+        LDRB R2, [R1, R5]
+        CMP R2, #52             // Compares to "4"
+        BEQ Try_nine
+        B Next_EID
+Try_nine: 
+        ADDS R5, #1             // Next character
+        LDRB R2, [R1, R5]
+        CMP R2, #57             // Compares to "9"
+        BEQ Try_two_two
+        B Next_EID
+Try_two_two: 
+        ADDS R5, #1             // Next character
+        LDRB R2, [R1, R5]
+        CMP R2, #50             // Compares to 2
+        BEQ Try_three
+        B Next_EID
+Try_three: 
+        ADDS R5, #1             // Next character
+        LDRB R2, [R1, R5]
+        CMP R2, #51             // Compares to 3
+        BEQ EID_Found
+        B Next_EID
+Next_EID:
+        ADDS R0, #8             // Increments the EID pointer to next EID
+        MOVS R1, #0             // Resets the letter pointer
+        MOVS R5, #0             // Resets the letter offset
+        LDR R1, [R0]            // Check if the EID is 0 to prevent Hardstop
+        CMP R1, #0
+        BEQ No_EID
+        B EID_one
+
+No_EID:
+        LDR R0, =-1             // Returns -1 in R0
+        B END
+EID_Found: 
+        ADDS R0, #4             // Increments to the EID's score
+        LDR R0, [R0]            // Returns score in R0
+        B END
+END:
+      POP  {R1-R7,PC}   // return
+       
+
+
+        .align 2
+        .global myClass
+myClass: .long pAB123  // pointer to EID
+         .long 95      // Score
+         .long pXYZ1   // pointer to EID
+         .long 96      // Score
+         .long pAB5549 // pointer to EID
+         .long 94      // Score
+         .long 0       // null pointer means end of list
+         .long 0
+pAB123:  .string "AB123"
+pXYZ1:   .string "XYZ1"
+pAB5549: .string "AB5549"
+        .end
